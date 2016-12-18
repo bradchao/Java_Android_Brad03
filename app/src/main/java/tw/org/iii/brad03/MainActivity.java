980da9a0f91a4a2ddf2c174bb00fa60916509f7f
brad <brad@brad.tw>
2016-12-18 15:56:58 +0800

    v4

--- app/src/main/java/tw/org/iii/brad03/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad03/MainActivity.java
@@ -2,12 +2,26 @@ package tw.org.iii.brad03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        initWebView();
     }
+    private void initWebView(){
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/map.html");
+    }
+
 }
