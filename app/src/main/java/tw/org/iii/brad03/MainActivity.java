665d184a48d70295aa63bfe6376d363be5a1d107
brad <brad@brad.tw>
2016-12-24 14:12:04 +0800

    v10

--- app/src/main/java/tw/org/iii/brad03/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad03/MainActivity.java
@@ -2,6 +2,8 @@ package tw.org.iii.brad03;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -17,6 +19,7 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private LocationManager lmgr;
+    private MyListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,36 +29,69 @@ public class MainActivity extends AppCompatActivity {
         // 檢查是否取的授權
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
+                != PackageManager.PERMISSION_GRANTED) {
             // 沒有授權, 所以要要求詢問授權
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     123);   // 123 => requestCode
-        }else{
+        } else {
             initLocation();
         }
 
-        webView = (WebView)findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
         initWebView();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 123){
+        if (requestCode == 123) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // get a grant
                 initLocation();
-            }else{
+            } else {
                 finish();
             }
         }
     }
 
-    private void initLocation(){
+    private void initLocation() {
         lmgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new MyListener();
+        lmgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (lmgr != null) {
+            lmgr.removeUpdates(listener);
+        }
+    }
+
+    private class MyListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+
 
     private void initWebView(){
         webView.setWebViewClient(new WebViewClient());
