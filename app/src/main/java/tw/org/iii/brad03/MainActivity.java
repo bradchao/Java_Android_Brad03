18441585d05e68f3eb432a646b8fabaef49abb9b
brad <brad@brad.tw>
2016-12-24 15:24:06 +0800

    v15

--- app/src/main/java/tw/org/iii/brad03/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad03/MainActivity.java
@@ -1,6 +1,7 @@
 package tw.org.iii.brad03;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,6 +11,7 @@ import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -127,10 +129,29 @@ public class MainActivity extends AppCompatActivity {
             super.handleMessage(msg);
             switch (msg.what){
                 case 0:
-                    Log.d("brad", mesg);
+                    //Log.d("brad", mesg);
+                    showAlert();
                     break;
             }
         }
     }
+
+    private void showAlert(){
+        AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Warn");
+        builder.setMessage(mesg);
+
+        dialog = builder.create();
+        dialog.show();
+    }
+    private void showConfirm(){
+
+    }
+    private void showPrompt(){
+
+    }
+
+
 
 }
