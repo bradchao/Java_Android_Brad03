df8ad0bd5fe1535745786fc2eb0da1a49774730b
brad <brad@brad.tw>
2016-12-24 13:42:20 +0800

    v7

--- app/src/main/java/tw/org/iii/brad03/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad03/MainActivity.java
@@ -1,5 +1,8 @@
 package tw.org.iii.brad03;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 檢查是否取的授權
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            // 沒有授權, 所以要要求詢問授權
+
+        }
 
         webView = (WebView)findViewById(R.id.webview);
         initWebView();
