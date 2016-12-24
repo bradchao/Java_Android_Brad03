9e7e9dce5f5aa0c884b65705776bb2805075906b
brad <brad@brad.tw>
2016-12-24 13:55:42 +0800

    v8

--- app/src/main/java/tw/org/iii/brad03/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad03/MainActivity.java
@@ -2,9 +2,12 @@ package tw.org.iii.brad03;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -23,12 +26,29 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED){
             // 沒有授權, 所以要要求詢問授權
-
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    123);   // 123 => requestCode
         }
 
         webView = (WebView)findViewById(R.id.webview);
         initWebView();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // get a grant
+                Log.d("brad", "got it");
+            }else{
+                finish();
+            }
+        }
+    }
+
     private void initWebView(){
         webView.setWebViewClient(new WebViewClient());
         WebSettings settings = webView.getSettings();
